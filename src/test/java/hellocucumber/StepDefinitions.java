7cd0ef1234b950036005038e3feb26d841af03b9
james arasim <jarasim@loandepot.com>
2022-09-28 11:59:02 -0700

    started glue file

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -6,16 +6,25 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("I have one BlackJack card with the value {int}")
+    public void i_have_one_black_jack_card_with_the_value(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @Given("another BlackJack card with the value {int}")
+    public void another_black_jack_card_with_the_value(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @When("I combine the card values")
+    public void i_combine_the_card_values() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("then the total value of my hand should be {int}")
+    public void then_the_total_value_of_my_hand_should_be(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 }
