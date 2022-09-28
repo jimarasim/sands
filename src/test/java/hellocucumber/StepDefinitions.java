cf64ab358e01245226608d52ca2bd9355e11b3db
james arasim <jarasim@loandepot.com>
2022-09-28 12:11:32 -0700

    first test complete

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -1,30 +1,28 @@
 package hellocucumber;
 
 import io.cucumber.java.en.*;
-
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinitions {
-
+    static int card1 = 0;
+    static int card2 = 0;
+    static int handValue = 0;
     @Given("I have one BlackJack card with the value {int}")
     public void i_have_one_black_jack_card_with_the_value(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        card1 = int1;
     }
     @Given("another BlackJack card with the value {int}")
     public void another_black_jack_card_with_the_value(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        card2 = int1;
     }
     @When("I combine the card values")
     public void i_combine_the_card_values() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        handValue = card1 + card2;
     }
     @Then("then the total value of my hand should be {int}")
     public void then_the_total_value_of_my_hand_should_be(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        int expectedHandValue = card1 + card2;
+        assertEquals(expectedHandValue, int1, "Incorrect Blackjack Hand value " + int1 + "for one Blackjack card " + card1 + " and another Blackjaeck card " + card2);
     }
 
 }
